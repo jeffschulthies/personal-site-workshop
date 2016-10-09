4b91f8eb37777a47e7d8c950bbe82b54670fae66
Jeff Schulthies <jeffschulthies@gwu.edu>
2016-10-08 23:44:04 -0400

    commit

--- css/css-start.sh
+++ css/css-start.sh
@@ -1,1 +1,3 @@
-sass --watch style.scss:style.css
+#sass --watch style.scss:style.css
+#sass --watch style-mobile.scss:style-mobile.css
+sass --watch ./desktop/*.scss ./mobile/*.scss ./partials/*.scss
